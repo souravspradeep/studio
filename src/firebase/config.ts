9c8f82fabf6879eaaa4c4609f4f0066c8201eb3e
Sourav S Pradeep <sourav.spradeep@gmail.com>
2025-10-29 15:50:49 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "campus-compass-p5kpr",
-  "appId": "1:1551289454:web:dbc595db13f9eeff0eaa7f",
-  "apiKey": "AIzaSyCseMMw7uweuEfcM9tAmOKAW1YYHSnlLvM",
-  "authDomain": "campus-compass-p5kpr.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1551289454"
+  apiKey: "AIzaSyCseMMw7uweuEfcM9tAmOKAW1YYHSnlLvM",
+  authDomain: "campus-compass-p5kpr.firebaseapp.com",
+  projectId: "campus-compass-p5kpr",
+  storageBucket: "campus-compass-p5kpr.firebasestorage.app",
+  messagingSenderId: "1551289454",
+  appId: "1:1551289454:web:dbc595db13f9eeff0eaa7f"
 };
